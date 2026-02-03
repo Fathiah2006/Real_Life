49389d0fa2a667d892f68565318a0d8cccfd531a
Fathiah <fathiahkazeem@gmail.com>
2026-02-03 16:41:05 +0100

    Fix import path

--- my_site/src/pages/home/Home.jsx
+++ my_site/src/pages/home/Home.jsx
@@ -5,11 +5,11 @@ import Services from './services/Services';
 import Partners from './partners/Partners';
 import About from '../about/About';
 import TechStack from './teckstack/TechStack';
-import Industry from './industry/Industry';
 import AdvancedTech from './advancetech/AdvancedTech';
 import WhyVonCross from './whyvoncross/WhyVoncross';
 import ValuesSection from './values/ValuesSection';
 import LetsStartForm from './form/LetsStartForm';
+import Industry from './industry/Industry'; 
 import { Import } from 'lucide-react';
 const Home = () => {
   return (
